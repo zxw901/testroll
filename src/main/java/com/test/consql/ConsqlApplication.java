82cbbc3ea8d44e1e86586979cfacd33f6af249e6
ZZZ0011 <45975043+ZZZ0011@users.noreply.github.com>
2019-05-22 16:10:02 +0800

    快照1

---
+++ src/main/java/com/test/consql/ConsqlApplication.java
@@ -0,0 +1,13 @@
+package com.test.consql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsqlApplication.class, args);
+	}
+
+}
